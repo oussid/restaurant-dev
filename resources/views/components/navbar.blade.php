c55f7ab877ded85a5e9394192ba851c05025faee
Oussama <idsaidgoldman@gmail.com>
2023-04-14 01:41:42 +0200

    updated navbar + created static carousel

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,33 @@
+
+<nav class="main-navbar">
+    <div class="main-navbar-left">
+     LOGO
+    </div>
+    <div class="main-navbar-center">
+       <a href="#" class="main-navbar-center-item">
+         HOME       
+       </a>
+       <a href="#" class="main-navbar-center-item">
+         MENU
+       </a>
+       <a href="#" class="main-navbar-center-item">
+         BOOK A TABLE
+       </a>
+       <a href="#" class="main-navbar-center-item">
+         CONTACT
+       </a>
+    </div>
+    <div class="main-navbar-right">
+         <div class="main-navbar-right-left">
+             <a href="#" class="main-navbar-right-left-item border-right">
+                 <i class="fa-solid fa-magnifying-glass"></i>
+             </a>
+             <a href="#" class="main-navbar-right-left-item">
+                 <i class="fa-solid fa-cart-shopping"></i>   
+             </a>
+         </div>
+         <div class="main-navbar-right-right">
+             <button>Login</button>
+         </div>
+    </div>
+ </nav>
